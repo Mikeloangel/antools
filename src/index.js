a6cbc66265f7125a9c35e7750230797abd1790a5
Mikhail Varushichev <mikeloangel@gmail.com>
2022-12-06 13:58:08 +0100

    feat: fonts set

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './index.scss';
 
-
 // easy FUOC fix (see index.html style tag)
 function handleFuoc(){
   let body = document.querySelector('body');
   body.style.visibility = 'visible';
 }
 
-window.addEventListener('load', handleFuoc)
+window.addEventListener('load', handleFuoc);
